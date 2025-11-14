69e0645ab9985dd7708e878e85addb4cc10c621f
Puji Mahendra <puji.mhdr@gmail.com>
2025-11-15 00:33:12 +0700

    Add new database

--- public/database/percobaan_1.sql
+++ public/database/percobaan_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 14, 2025 at 05:21 PM
+-- Generation Time: Nov 14, 2025 at 05:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -29,11 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mahasiswa` (
   `id` int(11) NOT NULL,
-  `nim` varchar(70) DEFAULT '000',
+  `nim` varchar(70) DEFAULT '00000',
   `nama` varchar(70) DEFAULT NULL,
   `prodi` varchar(70) DEFAULT NULL,
   `pt` varchar(70) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `prodi`, `pt`) VALUES
+(1, '210403020015', 'Zidane Zainudin', 'Pendidikan Otomotif', 'Universitas Pendidikan Jatim'),
+(2, '210403020015', 'Afala Riska Wati', 'Pendidikan Otomotif', 'Universitas Pendidikan Jatim');
 
 --
 -- Indexes for dumped tables
@@ -44,6 +52,16 @@ CREATE TABLE `mahasiswa` (
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
